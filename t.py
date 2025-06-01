4eda5e599df30844a858ee17a92d641cff4e86f9
Prayaan1711 <spyfyi1711@gmail.com>
2025-06-01 09:51:12 +0530

    1234

---
+++ t.py
@@ -0,0 +1,8 @@
+from datetime import time
+
+Time = time(11, 34, 56)
+
+print("hour = ", Time.hour)
+print("minute = ", Time.minute)
+print("seconds = ", Time.second)
+print("microseconds = ", Time.microsecond)
---
+++ t2.py
@@ -0,0 +1,8 @@
+from datetime import datetime as DT
+
+Time = DT.now()
+
+print("hour = ", Time.hour)
+print("minute = ", Time.minute)
+print("seconds = ", Time.second)
+print("microseconds = ", Time.microsecond)
